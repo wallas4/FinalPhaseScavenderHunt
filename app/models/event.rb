5d2e201c4f8001422b96f396706a517c18870fd8
Guymon Hall <guymon.hall@localhost.localdomain>
2015-04-10 09:12:33 -0700

    CS 270, Spring 2015, QR Code Scavenger Hunt, Phase 5

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ActiveRecord::Base
     has_and_belongs_to_many :locations
+    has_and_belongs_to_many :participants
+    has_one :roster
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,5 @@
 class Location < ActiveRecord::Base
     has_and_belongs_to_many :events
+    has_many :rosters
+    has_many :participants, :through => :rosters
 end
